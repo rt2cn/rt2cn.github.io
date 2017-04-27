462490c1ae595bae59b9614512bdd9423d60dc44
rt2cn <error4xx@gmail.com>
2017-04-27 17:31:23 +0800

    first commit

---
+++ data/js/payload_details.js
@@ -0,0 +1,5 @@
+var payload_details =  {
+  "tweets" : 0,
+  "created_at" : "2016-08-30 08:49:08 +0000",
+  "lang" : "zh-cn"
+}
---
+++ data/js/user_details.js
@@ -0,0 +1,7 @@
+var user_details =  {
+  "screen_name" : "RT2CN",
+  "full_name" : "RT2CN",
+  "bio" : "Tweets to China.",
+  "id" : "770449590827057155",
+  "created_at" : "2016-08-30 02:34:31 +0000"
+}
