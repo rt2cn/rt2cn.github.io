e58e6812e34f3c5c42f51046631fae059f34132d
RTBot <rt2cn@users.noreply.github.com>
2017-04-28 09:28:20 +0800

    Update tweet_index.js

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -12,5 +12,12 @@ var tweet_index = [
 		"var_name": "tweets_2016_10",
 		"tweet_count": 1,
 		"month": 10
+	},
+	{
+		"file_name": "data/js/tweets/2016_08.js",
+		"year": 2016,
+		"var_name": "tweets_2016_08",
+		"tweet_count": 1,
+		"month": 08
 	}
 ]
