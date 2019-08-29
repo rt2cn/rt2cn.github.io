4fb6b1f47f7fab8dacdb50e48f6e58f70da401fd
RTBot <rt2cn@users.noreply.github.com>
2019-08-29 03:37:43 +0000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10,
-	"created_at": "Thu Aug 29 03:34:30 +0000 2019",
+	"tweets": 12,
+	"created_at": "Thu Aug 29 03:37:42 +0000 2019",
 	"lang": "zh-cn"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,561 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Thu Aug 29 03:37:09 +0000 2019",
+		"id": 1166500356039168000,
+		"id_str": "1166917677945950208",
+		"full_text": "If you can’t be in HK, the Lennon Wall in Taipei is incredibly moving, and I was proud to see @badiucao art everywhere. 香港加油 https://t.co/rTfBMMZFk9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			124
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "badiucao",
+					"name": "巴丢草  Badiucao",
+					"id": 267695291,
+					"id_str": "267695291",
+					"indices": [
+						94,
+						103
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1166500329224982500,
+					"id_str": "1166500329224982528",
+					"indices": [
+						125,
+						148
+					],
+					"media_url": "http://pbs.twimg.com/media/EDA93nnU0AAooml.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDA93nnU0AAooml.jpg",
+					"url": "https://t.co/rTfBMMZFk9",
+					"display_url": "pic.twitter.com/rTfBMMZFk9",
+					"expanded_url": "https://twitter.com/natashaskassam/status/1166500356039168000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1166500329224982500,
+					"id_str": "1166500329224982528",
+					"indices": [
+						125,
+						148
+					],
+					"media_url": "http://pbs.twimg.com/media/EDA93nnU0AAooml.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDA93nnU0AAooml.jpg",
+					"url": "https://t.co/rTfBMMZFk9",
+					"display_url": "pic.twitter.com/rTfBMMZFk9",
+					"expanded_url": "https://twitter.com/natashaskassam/status/1166500356039168000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1166500329229185000,
+					"id_str": "1166500329229185024",
+					"indices": [
+						125,
+						148
+					],
+					"media_url": "http://pbs.twimg.com/media/EDA93noU8AAONiC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDA93noU8AAONiC.jpg",
+					"url": "https://t.co/rTfBMMZFk9",
+					"display_url": "pic.twitter.com/rTfBMMZFk9",
+					"expanded_url": "https://twitter.com/natashaskassam/status/1166500356039168000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1166500329225015300,
+					"id_str": "1166500329225015297",
+					"indices": [
+						125,
+						148
+					],
+					"media_url": "http://pbs.twimg.com/media/EDA93nnVUAEw3aO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDA93nnVUAEw3aO.jpg",
+					"url": "https://t.co/rTfBMMZFk9",
+					"display_url": "pic.twitter.com/rTfBMMZFk9",
+					"expanded_url": "https://twitter.com/natashaskassam/status/1166500356039168000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 27399122,
+			"id_str": "27399122",
+			"name": "Natasha Kassam",
+			"screen_name": "natashaskassam",
+			"location": "Sydney, New South Wales",
+			"description": "Research Fellow @lowyinstitute doing the #lowypoll, formerly @dfat working on #fpwhitepaper & in Beijing & #RAMSI. RT ≠ like, other usual caveats apply.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 499,
+			"friends_count": 614,
+			"listed_count": 5,
+			"created_at": "Sun Mar 29 08:13:24 +0000 2009",
+			"favourites_count": 1047,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 308,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1097459572736835585/6nXiI9cH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1097459572736835585/6nXiI9cH_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 263,
+		"favorite_count": 661,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "If you can’t be in HK, the Lennon Wall in Taipei is incredibly moving, and I was proud to see @badiucao art everywhere. 香港加油 https://t.co/rTfBMMZFk9"
+	},
+	{
+		"created_at": "Thu Aug 29 03:37:05 +0000 2019",
+		"id": 1166402893118763000,
+		"id_str": "1166917662489944064",
+		"full_text": "愛在香港抗爭時\nLove of #HongKongProtesters\n#StandwithHongKong\n#FreeHongKong\n#AntiELAB\n#FollowBackHongKong\n#HongKong\n(James Pomfret/Reuters; Kin Cheung/AP; Reuters; Kim Kyung Hoon/Reuters) https://t.co/jpiywzVtOh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			180
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HongKongProtesters",
+					"indices": [
+						16,
+						35
+					]
+				},
+				{
+					"text": "StandwithHongKong",
+					"indices": [
+						36,
+						54
+					]
+				},
+				{
+					"text": "FreeHongKong",
+					"indices": [
+						55,
+						68
+					]
+				},
+				{
+					"text": "AntiELAB",
+					"indices": [
+						69,
+						78
+					]
+				},
+				{
+					"text": "FollowBackHongKong",
+					"indices": [
+						79,
+						98
+					]
+				},
+				{
+					"text": "HongKong",
+					"indices": [
+						99,
+						108
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1166402883090124800,
+					"id_str": "1166402883090124800",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/EC_lPgeUEAAxVxi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EC_lPgeUEAAxVxi.jpg",
+					"url": "https://t.co/jpiywzVtOh",
+					"display_url": "pic.twitter.com/jpiywzVtOh",
+					"expanded_url": "https://twitter.com/abeleung/status/1166402893118763008/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1366,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1166402883090124800,
+					"id_str": "1166402883090124800",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/EC_lPgeUEAAxVxi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EC_lPgeUEAAxVxi.jpg",
+					"url": "https://t.co/jpiywzVtOh",
+					"display_url": "pic.twitter.com/jpiywzVtOh",
+					"expanded_url": "https://twitter.com/abeleung/status/1166402893118763008/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1366,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1166402883090182100,
+					"id_str": "1166402883090182144",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/EC_lPgeU8AARMYd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EC_lPgeU8AARMYd.jpg",
+					"url": "https://t.co/jpiywzVtOh",
+					"display_url": "pic.twitter.com/jpiywzVtOh",
+					"expanded_url": "https://twitter.com/abeleung/status/1166402893118763008/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1166402883098513400,
+					"id_str": "1166402883098513408",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/EC_lPggUEAA5tki.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EC_lPggUEAA5tki.jpg",
+					"url": "https://t.co/jpiywzVtOh",
+					"display_url": "pic.twitter.com/jpiywzVtOh",
+					"expanded_url": "https://twitter.com/abeleung/status/1166402893118763008/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1280,
+							"h": 853,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1166402883186655200,
+					"id_str": "1166402883186655233",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/EC_lPg1VAAEWHs5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EC_lPg1VAAEWHs5.jpg",
+					"url": "https://t.co/jpiywzVtOh",
+					"display_url": "pic.twitter.com/jpiywzVtOh",
+					"expanded_url": "https://twitter.com/abeleung/status/1166402893118763008/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 792,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 449,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1279,
+							"h": 844,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 38143483,
+			"id_str": "38143483",
+			"name": "🦆🍗",
+			"screen_name": "abeleung",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1617,
+			"friends_count": 442,
+			"listed_count": 42,
+			"created_at": "Wed May 06 08:58:31 +0000 2009",
+			"favourites_count": 7296,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 23449,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/534363526257602560/W3GK7DS3_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/534363526257602560/W3GK7DS3_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/38143483/1421234568",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 276,
+		"favorite_count": 602,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "愛在香港抗爭時\nLove of #HongKongProtesters\n#StandwithHongKong\n#FreeHongKong\n#AntiELAB\n#FollowBackHongKong\n#HongKong\n(James Pomfret/Reuters; Kin Cheung/AP; Reuters; Kim Kyung Hoon/Reuters) https://t.co/jpiywzVtOh"
+	},
+	{
 		"created_at": "Thu Aug 29 03:34:08 +0000 2019",
 		"id": 1166764328361480200,
 		"id_str": "1166916919649275906",
