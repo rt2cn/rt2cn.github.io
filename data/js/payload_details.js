0d7c807c67221571b9b96fcc7deccaccef95d23c
RTBot <rt2cn@users.noreply.github.com>
2019-09-01 02:08:18 +0000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94,
-	"created_at": "Sun Sep 01 00:08:17 +0000 2019",
+	"tweets": 96,
+	"created_at": "Sun Sep 01 02:08:17 +0000 2019",
 	"lang": "zh-cn"
 }
---
+++ data/js/tweets/2019_09.js
@@ -0,0 +1,445 @@
+Grailbird.data.tweets_2019_09 = 
+[
+	{
+		"created_at": "Sun Sep 01 01:40:49 +0000 2019",
+		"id": 1167970897375817700,
+		"id_str": "1167975564470185984",
+		"full_text": "胡锡进：建信息防火墙是因为外国的实力强于中国，所以是有必要的！\n\n但他自己和混球时报的员工都可以随意翻墙，因为那是是工作需要\n\n这帮人是中国最坏的人，了解多少墙外真相都丝毫改变不了他们投机逐利的心，害人无数！\n恐怕最终不是绝症，就是横死，脏钱有的赚，没得花\n\n https://t.co/LJEqJBl1D6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			154
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1167822509015650300,
+					"id_str": "1167822509015650305",
+					"indices": [
+						131,
+						154
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1167822509015650305/pu/img/3JE-SsytCh7B6jcN.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1167822509015650305/pu/img/3JE-SsytCh7B6jcN.jpg",
+					"url": "https://t.co/LJEqJBl1D6",
+					"display_url": "pic.twitter.com/LJEqJBl1D6",
+					"expanded_url": "https://twitter.com/alicedreamss/status/1167823192188014592/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 624,
+							"h": 368,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 624,
+							"h": 368,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 624,
+							"h": 368,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1167823192188014600,
+					"source_status_id_str": "1167823192188014592",
+					"source_user_id": 565498274,
+					"source_user_id_str": "565498274"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1167822509015650300,
+					"id_str": "1167822509015650305",
+					"indices": [
+						131,
+						154
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1167822509015650305/pu/img/3JE-SsytCh7B6jcN.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1167822509015650305/pu/img/3JE-SsytCh7B6jcN.jpg",
+					"url": "https://t.co/LJEqJBl1D6",
+					"display_url": "pic.twitter.com/LJEqJBl1D6",
+					"expanded_url": "https://twitter.com/alicedreamss/status/1167823192188014592/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 624,
+							"h": 368,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 624,
+							"h": 368,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 624,
+							"h": 368,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1167823192188014600,
+					"source_status_id_str": "1167823192188014592",
+					"source_user_id": 565498274,
+					"source_user_id_str": "565498274",
+					"video_info": {
+						"aspect_ratio": [
+							39,
+							23
+						],
+						"duration_millis": 97120,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1167822509015650305/pu/pl/wDGPlEfoPL_5z5DS.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1167822509015650305/pu/vid/624x368/bgH2o22jHp_UUtYA.mp4?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1167822509015650305/pu/vid/456x270/Djl2_0ljx2oOGcFJ.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false,
+						"source_user": {
+							"id": 565498274,
+							"id_str": "565498274",
+							"name": "向莉",
+							"screen_name": "alicedreamss",
+							"location": "",
+							"description": "Xiang Li , Human rights defender , independent curator. For freedom. 向莉，人权捍卫者，独立策展人 。争取自由！Support Hong Kong！",
+							"url": null,
+							"entities": {
+								"description": {
+									"urls": []
+								}
+							},
+							"protected": false,
+							"followers_count": 36422,
+							"friends_count": 1480,
+							"listed_count": 146,
+							"created_at": "Sat Apr 28 14:52:15 +0000 2012",
+							"favourites_count": 7688,
+							"utc_offset": null,
+							"time_zone": null,
+							"geo_enabled": false,
+							"verified": false,
+							"statuses_count": 9957,
+							"lang": null,
+							"contributors_enabled": false,
+							"is_translator": false,
+							"is_translation_enabled": false,
+							"profile_background_color": "C0DEED",
+							"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+							"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+							"profile_background_tile": false,
+							"profile_image_url": "http://pbs.twimg.com/profile_images/1143360049454014464/8jnMYQ6c_normal.jpg",
+							"profile_image_url_https": "https://pbs.twimg.com/profile_images/1143360049454014464/8jnMYQ6c_normal.jpg",
+							"profile_banner_url": "https://pbs.twimg.com/profile_banners/565498274/1565624548",
+							"profile_link_color": "1DA1F2",
+							"profile_sidebar_border_color": "C0DEED",
+							"profile_sidebar_fill_color": "DDEEF6",
+							"profile_text_color": "333333",
+							"profile_use_background_image": true,
+							"has_extended_profile": true,
+							"default_profile": true,
+							"default_profile_image": false,
+							"following": false,
+							"follow_request_sent": false,
+							"notifications": false,
+							"translator_type": "none"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 338248117,
+			"id_str": "338248117",
+			"name": "新闻大吐槽",
+			"screen_name": "TuCaoFakeNews",
+			"location": "",
+			"description": "独特视角对比墙内外热点！ 吐槽可笑、可悲、可叹的墙内外“新闻”！欢迎转发！17-01-2019始 ！坚信智慧和脏话不共存，请文明发帖",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20392,
+			"friends_count": 4491,
+			"listed_count": 86,
+			"created_at": "Tue Jul 19 08:56:41 +0000 2011",
+			"favourites_count": 25331,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 14300,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1085013653089185793/E7suCzwt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1085013653089185793/E7suCzwt_normal.jpg",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 45,
+		"favorite_count": 152,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "zh",
+		"text": "胡锡进：建信息防火墙是因为外国的实力强于中国，所以是有必要的！\n\n但他自己和混球时报的员工都可以随意翻墙，因为那是是工作需要\n\n这帮人是中国最坏的人，了解多少墙外真相都丝毫改变不了他们投机逐利的心，害人无数！\n恐怕最终不是绝症，就是横死，脏钱有的赚，没得花\n\n https://t.co/LJEqJBl1D6"
+	},
+	{
+		"created_at": "Sun Sep 01 01:40:16 +0000 2019",
+		"id": 1167926792478322700,
+		"id_str": "1167975426955698178",
+		"full_text": "地铁是港人上街和返屋企主交通工具。共匪为阻港人上街示威费尽心机，选择避开媒体聚光镜此处以黑社会打手在地铁车厢内行凶恐吓港人，无果。盗国日庆典临近时间紧任务重，情急之下“港警”直接出场拘捕大打出手妇幼也不放过。天欲其亡必令其狂，香港基督徒祷告日匪共恶行上帝看到，世界灭共进程会加速！ https://t.co/6MIpJq3t2c",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			140
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1167926710760685600,
+					"id_str": "1167926710760685569",
+					"indices": [
+						141,
+						164
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1167926710760685569/pu/img/ijDaBf5xPFV3PEA_.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1167926710760685569/pu/img/ijDaBf5xPFV3PEA_.jpg",
+					"url": "https://t.co/6MIpJq3t2c",
+					"display_url": "pic.twitter.com/6MIpJq3t2c",
+					"expanded_url": "https://twitter.com/7k_QYS/status/1167926792478322693/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 320,
+							"h": 568,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 320,
+							"h": 568,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 320,
+							"h": 568,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1167926710760685600,
+					"id_str": "1167926710760685569",
+					"indices": [
+						141,
+						164
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1167926710760685569/pu/img/ijDaBf5xPFV3PEA_.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1167926710760685569/pu/img/ijDaBf5xPFV3PEA_.jpg",
+					"url": "https://t.co/6MIpJq3t2c",
+					"display_url": "pic.twitter.com/6MIpJq3t2c",
+					"expanded_url": "https://twitter.com/7k_QYS/status/1167926792478322693/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 320,
+							"h": 568,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 320,
+							"h": 568,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 320,
+							"h": 568,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							40,
+							71
+						],
+						"duration_millis": 51105,
+						"variants": [
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1167926710760685569/pu/vid/320x568/45F3A07jtJzV33h6.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1167926710760685569/pu/pl/4M5lzfrrVq717p5F.m3u8?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 864301084021366800,
+			"id_str": "864301084021366784",
+			"name": "邱岳首",
+			"screen_name": "7k_QYS",
+			"location": "",
+			"description": "支持爆料革命挺郭义工，“澳洲新声”小媒体客串打杂。https://t.co/BkaYsVmHVV\n \n  https://t.co/uKiTufoUpZ，",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/BkaYsVmHVV",
+							"expanded_url": "https://www.youtube.com/channel/UC66D8Bl3TeNTgyUAG-mcwHg/videos",
+							"display_url": "youtube.com/channel/UC66D8…",
+							"indices": [
+								25,
+								48
+							]
+						},
+						{
+							"url": "https://t.co/uKiTufoUpZ",
+							"expanded_url": "http://guo.media/Qiu8585",
+							"display_url": "guo.media/Qiu8585",
+							"indices": [
+								53,
+								76
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 45730,
+			"friends_count": 2337,
+			"listed_count": 127,
+			"created_at": "Tue May 16 02:06:51 +0000 2017",
+			"favourites_count": 24477,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15780,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1135673477191671808/0CJ12Osx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1135673477191671808/0CJ12Osx_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 217,
+		"favorite_count": 299,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "zh",
+		"text": "地铁是港人上街和返屋企主交通工具。共匪为阻港人上街示威费尽心机，选择避开媒体聚光镜此处以黑社会打手在地铁车厢内行凶恐吓港人，无果。盗国日庆典临近时间紧任务重，情急之下“港警”直接出场拘捕大打出手妇幼也不放过。天欲其亡必令其狂，香港基督徒祷告日匪共恶行上帝看到，世界灭共进程会加速！ https://t.co/6MIpJq3t2c"
+	}
+]
