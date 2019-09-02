b48b7d5678636c3f22d775adfdc7d17e27bd2a78
RTBot <rt2cn@users.noreply.github.com>
2019-09-02 14:08:19 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 137,
-	"created_at": "Mon Sep 02 04:08:18 +0000 2019",
+	"tweets": 138,
+	"created_at": "Mon Sep 02 14:08:18 +0000 2019",
 	"lang": "zh-cn"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Mon Sep 02 14:08:05 +0000 2019",
+		"id": 1167979460139683800,
+		"id_str": "1168526009693614081",
+		"full_text": "[GRAPHIC] Fuller video of the now-infamous train attack scene from HK Prince Edward station last night. It should be clear now @hkpoliceforce is no longer bounded by law in any meaningful sense of the word.\nCopyright CC 梁柏堅 Pakkin Leung @ Rice Post\n#HongKongProtests #antiELAB https://t.co/KdOJ7SZMaR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HongKongProtests",
+					"indices": [
+						249,
+						266
+					]
+				},
+				{
+					"text": "antiELAB",
+					"indices": [
+						267,
+						276
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "hkpoliceforce",
+					"name": "Hong Kong Police Force",
+					"id": 1057237461007396900,
+					"id_str": "1057237461007396864",
+					"indices": [
+						127,
+						141
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1167979262139228200,
+					"id_str": "1167979262139228160",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1167979262139228160/pu/img/QZYBBdHF1v_ZsuZm.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1167979262139228160/pu/img/QZYBBdHF1v_ZsuZm.jpg",
+					"url": "https://t.co/KdOJ7SZMaR",
+					"display_url": "pic.twitter.com/KdOJ7SZMaR",
+					"expanded_url": "https://twitter.com/PaulHuangReport/status/1167979460139683840/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1167979262139228200,
+					"id_str": "1167979262139228160",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1167979262139228160/pu/img/QZYBBdHF1v_ZsuZm.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1167979262139228160/pu/img/QZYBBdHF1v_ZsuZm.jpg",
+					"url": "https://t.co/KdOJ7SZMaR",
+					"display_url": "pic.twitter.com/KdOJ7SZMaR",
+					"expanded_url": "https://twitter.com/PaulHuangReport/status/1167979460139683840/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 139440,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1167979262139228160/pu/vid/1280x720/UUlFdA9_8Xye4ktP.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1167979262139228160/pu/vid/640x360/VuFLzrmAiaQLVr4n.mp4?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1167979262139228160/pu/vid/480x270/MmssK1O6JbHR37wu.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1167979262139228160/pu/pl/ybVQaJTDizvIcGWM.m3u8?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 736279751036264400,
+			"id_str": "736279751036264448",
+			"name": "Paul Huang",
+			"screen_name": "PaulHuangReport",
+			"location": "Kaohsiung City, Taiwan",
+			"description": "Investigative journalist and writer on East Asia, Taiwan, and China. Flying solo.\nEmail\npaul.huang.TW@protonmail.com",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1375,
+			"friends_count": 819,
+			"listed_count": 47,
+			"created_at": "Fri May 27 19:35:46 +0000 2016",
+			"favourites_count": 3147,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1706,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/917098573170343937/TESPmBx__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/917098573170343937/TESPmBx__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/736279751036264448/1507488152",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3743,
+		"favorite_count": 3634,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "[GRAPHIC] Fuller video of the now-infamous train attack scene from HK Prince Edward station last night. It should be clear now @hkpoliceforce is no longer bounded by law in any meaningful sense of the word.\nCopyright CC 梁柏堅 Pakkin Leung @ Rice Post\n#HongKongProtests #antiELAB https://t.co/KdOJ7SZMaR"
+	},
+	{
 		"created_at": "Mon Sep 02 03:15:53 +0000 2019",
 		"id": 1168360679494914000,
 		"id_str": "1168361879003156480",
