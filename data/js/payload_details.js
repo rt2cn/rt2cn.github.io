69668291c90e367cce903dc7fadd5019f1f26b58
RTBot <rt2cn@users.noreply.github.com>
2019-09-06 18:08:19 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 173,
-	"created_at": "Fri Sep 06 17:08:17 +0000 2019",
+	"tweets": 174,
+	"created_at": "Fri Sep 06 18:08:18 +0000 2019",
 	"lang": "zh-cn"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,195 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Fri Sep 06 17:29:55 +0000 2019",
+		"id": 1170022313498038300,
+		"id_str": "1170026353447624705",
+		"full_text": "當被捕人或者家長們事後傳來短訊說謝謝，然後詢問有沒有他們可以貢獻出一分力的地方——那種感覺再多錢也買不到。\n\n“The point is not to pay back kindness, but to pass it on.” Hongkongers, gayau :’)🖤\n#momentslikethis #PassItOn https://t.co/Ofqsv9XwfN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			165
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "momentslikethis",
+					"indices": [
+						139,
+						155
+					]
+				},
+				{
+					"text": "PassItOn",
+					"indices": [
+						156,
+						165
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1170022307089174500,
+					"id_str": "1170022307089174528",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/EDzBFtlVUAAv1uL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDzBFtlVUAAv1uL.jpg",
+					"url": "https://t.co/Ofqsv9XwfN",
+					"display_url": "pic.twitter.com/Ofqsv9XwfN",
+					"expanded_url": "https://twitter.com/mizrdg/status/1170022313498038273/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 491,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 542,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 542,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1170022307089174500,
+					"id_str": "1170022307089174528",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/EDzBFtlVUAAv1uL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDzBFtlVUAAv1uL.jpg",
+					"url": "https://t.co/Ofqsv9XwfN",
+					"display_url": "pic.twitter.com/Ofqsv9XwfN",
+					"expanded_url": "https://twitter.com/mizrdg/status/1170022313498038273/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 491,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 542,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 542,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 804212653,
+			"id_str": "804212653",
+			"name": "Miss Reading",
+			"screen_name": "mizrdg",
+			"location": "Hong Kong",
+			"description": "Barrister. Law Graduate @UniRDG_Law @HKUniversity. A born foodie. Any super power? I make bubble tea and french fries disappear.",
+			"url": "https://t.co/JwNHhT1FIo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/JwNHhT1FIo",
+							"expanded_url": "https://twitter.com/hkfoodporn",
+							"display_url": "twitter.com/hkfoodporn",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2230,
+			"friends_count": 147,
+			"listed_count": 63,
+			"created_at": "Wed Sep 05 09:14:25 +0000 2012",
+			"favourites_count": 9807,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3277,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1102279168287363072/fgy_WSGX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1102279168287363072/fgy_WSGX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/804212653/1562180288",
+			"profile_link_color": "805A46",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "F4DC34",
+			"profile_text_color": "0C7C66",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 21,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "zh",
+		"text": "當被捕人或者家長們事後傳來短訊說謝謝，然後詢問有沒有他們可以貢獻出一分力的地方——那種感覺再多錢也買不到。\n\n“The point is not to pay back kindness, but to pass it on.” Hongkongers, gayau :’)🖤\n#momentslikethis #PassItOn https://t.co/Ofqsv9XwfN"
+	},
+	{
 		"created_at": "Fri Sep 06 16:53:23 +0000 2019",
 		"id": 1168431388338442200,
 		"id_str": "1170017158107021312",
